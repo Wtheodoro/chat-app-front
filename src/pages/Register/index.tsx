45b262afa2264e21f1e57b7930cbac2b980d2245
Walison Theodoro <theodoro.walison@gmail.com>
2022-07-24 18:53:43 -0300

    feat: login form

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FormContainter } from './styles'
+import Logo from '../../assets/logo.svg'
 
 const Register: React.FC = () => {
-  return <div>Register</div>
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+    alert('form')
+  }
+
+  const handleChange = (event: any) => {}
+
+  return (
+    <FormContainter>
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <div className='brand'>
+          <img src={Logo} />
+          <h1>Snappy</h1>
+        </div>
+
+        <input
+          typeof='text'
+          placeholder='Username'
+          name='username'
+          onChange={(event) => handleChange(event)}
+        />
+        <input
+          typeof='email'
+          placeholder='Email'
+          name='email'
+          onChange={(event) => handleChange(event)}
+        />
+        <input
+          typeof='password'
+          placeholder='Password'
+          name='password'
+          onChange={(event) => handleChange(event)}
+        />
+        <input
+          typeof='password'
+          placeholder='Confirm Password'
+          name='confirmPassword'
+          onChange={(event) => handleChange(event)}
+        />
+
+        <button type='submit'>Create User</button>
+        <span>
+          Already have an account? <Link to='/login'>Login</Link>
+        </span>
+      </form>
+    </FormContainter>
+  )
 }
 
 export default Register
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,80 @@
+import styled from 'styled-components'
+
+export const FormContainter = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  background: #131324;
+
+  .brand {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    img {
+      height: 5rem;
+    }
+
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    background: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+
+    input {
+      background: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      columns: white;
+      width: 100%;
+      font-size: 1rem;
+
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+
+    button {
+      background: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      border-radius: 0.4rem;
+      font-size: 1rem;
+      text-transform: uppercase;
+      transition: 0.3s ease-in-out;
+
+      &:hover {
+        background: #4e0eff;
+      }
+    }
+
+    span {
+      color: white;
+      text-transform: uppercase;
+
+      a {
+        color: #4e0eff;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+  }
+`
